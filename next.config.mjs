74456146c4c21ec09cada19e425a9c0244962a13
Adrian De Souza <67642190+Daikilaw@users.noreply.github.com>
2024-05-15 19:57:12 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,12 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks.maxSize = 200000; // Ajustar el tamaño de los chunks para reducir el uso de memoria
+    }
+    return config;
+  },
+};
